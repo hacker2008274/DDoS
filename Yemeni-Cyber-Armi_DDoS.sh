2958b05fbbfe793027ce3064b64b718b70335954
PhamChien_19994038 <99984318+hacker2008274@users.noreply.github.com>
2022-08-06 11:12:14 +0700

    Create Yemeni-Cyber-Armi_DDoS.sh

---
+++ Yemeni-Cyber-Armi_DDoS.sh
@@ -0,0 +1,16 @@
+#copyright : @yemen_cyber_armi_vietnam
+# DDoS Tools Copright : Yemeni Cyber Armi VietNam
+#github : https://github.com/hacker2008274
+#telegram : 0978842516:@yemen_cyber_armi_vietnam
+#we are Yemeni Cyber Armi Vietnamese
+
+clear
+figlet DDoS Yemen tools
+echo -e "\e[1;34m[:::] ...::: DDoS Tools Copright : Yemeni Cyber Armi VietNam :::... [:::]"
+echo -e "\e[1;34m[:::] ...::: github : https://github.com/hacker2008274       :::... [:::]"
+echo -e "\e[1;34m[:::] ...::: telegram : 0978842516:@yemen_cyber_armi_vietnam :::... [:::]"
+echo -e "\e[1;34m[:::] Target (ex : https://url.com/) : "
+read target
+echo -e "\e[1;34m[:::] we are Yemeni Cyber Armi Vietnamese"
+sleep 5
+go run 1.go -site $target
