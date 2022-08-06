0e21a4f7804bf0e159e421a13ad7ed9597d75240
PhamChien_19994038 <99984318+hacker2008274@users.noreply.github.com>
2022-08-06 18:42:06 +0700

    Update Yemeni-Cyber-Armi_DDoS.sh

--- Yemeni-Cyber-Armi_DDoS.sh
+++ Yemeni-Cyber-Armi_DDoS.sh
@@ -1,14 +1,14 @@
-#copyright : @yemen_cyber_armi_vietnam
-# DDoS Tools Copright : Yemeni Cyber Armi VietNam
+#copyright : @an_danh
+# DDoS Tools Copright : an_danh
 #github : https://github.com/hacker2008274
-#telegram : 0978842516:@yemen_cyber_armi_vietnam
-#we are Yemeni Cyber Armi Vietnamese
+#telegram : 0978842516:@an_danh
+#we are no_team
 
 clear
-figlet DDoS Yemen tools
-echo -e "\e[1;34m[:::] ...::: DDoS Tools Copright : Yemeni Cyber Armi VietNam :::... [:::]"
-echo -e "\e[1;34m[:::] ...::: github : https://github.com/hacker2008274       :::... [:::]"
-echo -e "\e[1;34m[:::] ...::: telegram : 0978842516:@yemen_cyber_armi_vietnam :::... [:::]"
+figlet DDoS 
+echo -e "\e[1;34m[:::] ...::: DDoS Tools Copright : No team.                   :::... [:::]"
+echo -e "\e[1;34m[:::] ...::: github : https://github.com/hacker2008274        :::... [:::]"
+echo -e "\e[1;34m[:::] ...::: telegram : 0978842516:@an_danh.                  :::... [:::]"
 echo -e "\e[1;34m[:::] Target (ex : https://url.com/) : "
 read target
 echo -e "\e[1;34m[:::] we are Yemeni Cyber Armi Vietnamese"
